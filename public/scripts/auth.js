0091623698311c1b449ab3a304c6ba9300a1a3ad
Ofek <ofek@outlook.com>
2016-07-18 17:23:37 +0300

    add password feedback

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,7 +1,7 @@
 function attemptAuth(event) {
     if (event.preventDefault) {
         event.preventDefault();
-    } 
+    }
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     firebase.auth().signInWithEmailAndPassword(email, password)
@@ -9,6 +9,11 @@ function attemptAuth(event) {
         window.location.href = '/index.html';
     }).catch(function (error) {
         if (error && error.code === 'auth/user-not-found') {
+            if (password.length < 7) {
+                document.getElementById('error').textContent = 'על הסיסמה לכלול לפחות 7 תווים';
+                return false;
+            }
+
             return firebase.auth().createUserWithEmailAndPassword(email, password).then(function (user) {
                 firebase.database().ref('/users/' + email.split('.').join(',')).set({
                     uid: user.uid
@@ -19,6 +24,11 @@ function attemptAuth(event) {
                 console.log(error.code, error.message);
             });
         } else if (error) {
+
+            if (error.code === 'auth/wrong-password') {
+                document.getElementById('error').textContent = 'סיסמה לא נכונה';
+            }
+
             console.log(error.code, error.message);
         }
     });
