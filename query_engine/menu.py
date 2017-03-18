4443ef9e33baaedec4191e8d2928554787f3f19c
guy <guyziv2110@gmail.com>
2017-03-18 14:08:24 +0200

    added one more query (still not completed)

--- query_engine/menu.py
+++ query_engine/menu.py
@@ -1,8 +1,7 @@
 import queries
 from colors import ColorEngine
+import exceptions
 
-
-# consider adding color handler instead of hardcoded ANSI escapes.
 def count_n_arrivals():
     print("Choose lower bound N (the times of arrival): ")
     n = input()
@@ -10,15 +9,29 @@ def count_n_arrivals():
     ColorEngine.print('Overall {0} volunteers arrived at least {1} times.'.format(res, n), ColorEngine.BOLD)
 
 
+def count_inactive_x_weeks():
+    option_is_in_development()
+    #print("Choose lower bound N (the inactive period in weeks): ")
+    #n = input()
+    #res = queries.count_inactive_x_weeks(n)
+    #ColorEngine.print('Overall {0} volunteers were inactive for at least {1} weeks.'.format(res, n), ColorEngine.BOLD)
+
+
 def my_quit_fn():
     raise SystemExit
+
+
+def option_is_in_development():
+    print("This option is still in development")
+    raise exceptions.InDevelopment
 
 
 def invalid():
     print("INVALID CHOICE!")
 
 menu = {"1": ("Count how many volunteers arrived N times", count_n_arrivals),
-        "2": ("Quit",my_quit_fn)}
+        "2": ("Count how many volunteers were inactive for atleast N weeks", count_inactive_x_weeks),
+        "3": ("Quit",my_quit_fn)}
 
 
 def show_menu():
@@ -26,6 +39,9 @@ def show_menu():
         ColorEngine.print(key+":" + menu[key][0], [ColorEngine.BLUE, ColorEngine.BOLD])
 
     while(True):
-        print("Make A Choice:")
-        ans = input()
-        menu.get(ans,[None, invalid])[1]()
+        try:
+            print("Make A Choice:")
+            ans = input()
+            menu.get(ans,[None, invalid])[1]()
+        except exceptions.InDevelopment as e:
+            pass
--- query_engine/queries.py
+++ query_engine/queries.py
@@ -1,16 +1,16 @@
 import mediator
-
+from datetime import timedelta, datetime
 
 # count how many users volunteered N times
-def count_n_arrivals(N):
-    N = int(N)
+def count_n_arrivals(n):
+    n = int(n)
     connection = mediator.connection.get_connection()
     users = connection.child("users").get()
     count = 0
     for user in users.each():
         try:
             attended = user.val().get("attended")
-            if user.val().get("attended") is not None and len(attended) >= N:
+            if user.val().get("attended") is not None and len(attended) >= n:
                 count += 1
         except Exception as e:
             # trace error - print("Login failed, error: {0}".format(e))
@@ -18,3 +18,25 @@ def count_n_arrivals(N):
 
     return count
 
+
+# count how many volunteers were inactive for at least N weeks
+def count_inactive_x_weeks(n):
+    n = int(n)
+    connection = mediator.connection.get_connection()
+    users = connection.child("users").get()
+    count = 0
+    for user in users.each():
+        try:
+            attended = user.val().get("attended")
+            if user.val().get("attended") is not None and len(attended) >= n:
+                last_attended = get_last_attended(attended)
+        except Exception as e:
+            # trace error - print("Login failed, error: {0}".format(e))
+            pass
+
+    return count
+
+
+# helpers
+def get_last_attended(attended):
+    return max(d for d in attended.values())
