b97866377d7c7a9b5276a53366ce1080b69eaf30
Guy Ziv <guyziv2110@gmail.com>
2017-04-01 20:26:15 +0300

    add service account key + more queries

--- query_engine/menu.py
+++ query_engine/menu.py
@@ -2,11 +2,14 @@ import queries
 from colors import ColorEngine
 import exceptions
 
-def count_n_arrivals():
+
+def count_and_print_n_arrivals():
     print("Choose lower bound N (the times of arrival): ")
     n = input()
-    res = queries.count_n_arrivals(n)
-    ColorEngine.print('Overall {0} volunteers arrived at least {1} times.'.format(res, n), ColorEngine.BOLD)
+    res = queries.count_and_print_n_arrivals(n)
+    ColorEngine.print('Overall {0} volunteers arrived at least {1} times:'.format(len(res), n), ColorEngine.BOLD)
+    for name in res:
+        print(name)
 
 
 def count_inactive_x_weeks():
@@ -14,14 +17,30 @@ def count_inactive_x_weeks():
     print("Choose lower bound N (the inactive period in weeks): ")
     n = input()
     res = queries.count_inactive_x_weeks(n)
-    ColorEngine.print('Overall {0} volunteers were inactive for at least {1} weeks.'.format(res, n), ColorEngine.BOLD)
+    ColorEngine.print('Overall {0} volunteers were inactive for at least {1} weeks:'.format(len(res), n), ColorEngine.BOLD)
+    for name in res:
+        print(name)
 
 
 def came_x_times_and_never_returned():
     print("Choose lower bound N (the times of arrival before never returned): ")
     n = input()
     res = queries.came_x_times_and_never_returned(n)
-    ColorEngine.print('Overall {0} volunteers came {1} weeks and then never returned.'.format(res, n), ColorEngine.BOLD)
+    ColorEngine.print('Overall {0} volunteers came {1} weeks and then never returned:'.format(len(res), n), ColorEngine.BOLD)
+    for name in res:
+        print(name)
+
+
+def average_of_arrivals_in_n_weeks():
+    print("Choose lower bound N indicating the last n weeks to calculate the average on: ")
+    n = input()
+    res = queries.average_of_arrivals_in_n_weeks(n)
+    for r in res:
+        arrival_avg = r[1] * 100
+        if arrival_avg > 100:
+            arrival_avg = 100 # for cases where volunteer came more than one time in a week.
+        ColorEngine.print('Volunteer: {0}, average of arrivals in the last {1} weeks is: {2:.0f}%.'
+                          .format(r[0], n, arrival_avg), [])
 
 
 def my_quit_fn():
@@ -36,11 +55,12 @@ def option_is_in_development():
 def invalid():
     print("INVALID CHOICE!")
 
-menu = {"1": ("Count how many volunteers arrived N times", count_n_arrivals),
+menu = {"1": ("Count and display how many volunteers arrived N times", count_and_print_n_arrivals),
         "2": ("Count how many volunteers were inactive for at least N weeks", count_inactive_x_weeks),
         "3": ("Count how many volunteers came at least N weeks and never returned (were inactive for at least 3 weeks)",
                 came_x_times_and_never_returned),
-        "4": ("Quit",my_quit_fn)}
+        "4": ("Average of arrivals in the last N weeks", average_of_arrivals_in_n_weeks),
+        "5": ("Quit",my_quit_fn)}
 
 
 def show_menu():
--- query_engine/queries.py
+++ query_engine/queries.py
@@ -1,22 +1,24 @@
 import mediator
 from datetime import timedelta, datetime
 
+
 # count how many users volunteered N times
-def count_n_arrivals(n):
+def count_and_print_n_arrivals(n):
     n = int(n)
     connection = mediator.connection.get_connection()
     users = connection.child("users").get()
-    count = 0
+    users_list = []
     for user in users.each():
         try:
             attended = user.val().get("attended")
             if user.val().get("attended") is not None and len(attended) >= n:
-                count += 1
+                user_profile = user.val().get('profile')
+                users_list.append('{0} {1}.'.format(user_profile.get('firstName'), user_profile.get('lastName')))
         except Exception as e:
             # trace error - print("Login failed, error: {0}".format(e))
             pass
 
-    return count
+    return users_list
 
 
 # count how many volunteers were inactive for at least N weeks
@@ -24,7 +26,7 @@ def count_inactive_x_weeks(n):
     n = int(n)
     connection = mediator.connection.get_connection()
     users = connection.child("users").get()
-    count = 0
+    users_list = []
     for user in users.each():
         try:
             attended = user.val().get("attended")
@@ -36,13 +38,14 @@ def count_inactive_x_weeks(n):
                 last_attended_days_delta = (last_attended_parsed - timedelta(days=last_attended_parsed.weekday()))
                 weeks_diff = (today_days_delta - last_attended_days_delta).days / 7
                 if weeks_diff >= n:
-                    count += 1
+                    user_profile = user.val().get('profile')
+                    users_list.append('{0} {1}.'.format(user_profile.get('firstName'), user_profile.get('lastName')))
 
         except Exception as e:
             # trace error - print("Login failed, error: {0}".format(e))
             pass
 
-    return count
+    return users_list
 
 
 # count how many volunteers came at least N weeks and never returned
@@ -50,7 +53,7 @@ def came_x_times_and_never_returned(n):
     n = int(n)
     connection = mediator.connection.get_connection()
     users = connection.child("users").get()
-    count = 0
+    users_list = []
     for user in users.each():
         try:
             attended = user.val().get("attended")
@@ -62,14 +65,48 @@ def came_x_times_and_never_returned(n):
                 last_attended_days_delta = (last_attended_parsed - timedelta(days=last_attended_parsed.weekday()))
                 weeks_diff = (today_days_delta - last_attended_days_delta).days / 7
                 if weeks_diff >= 3:
-                    count += 1
+                    user_profile = user.val().get('profile')
+                    users_list.append('{0} {1}.'.format(user_profile.get('firstName'), user_profile.get('lastName')))
 
         except Exception as e:
             # trace error - print("Login failed, error: {0}".format(e))
             pass
 
-    return count
+    return users_list
+
+
+def average_of_arrivals_in_n_weeks(n):
+    n = int(n)
+    connection = mediator.connection.get_connection()
+    users = connection.child("users").get()
+    count = 0
+    users_list = []
+    for user in users.each():
+        try:
+            attended = user.val().get("attended")
+            if user.val().get("attended") is not None:
+                user_profile = user.val().get('profile')
+                user_profile_name = ('{0} {1}.'.format(user_profile.get('firstName'), user_profile.get('lastName')))
+                user_tup = (user_profile_name, count_attendances_in_last_n_weeks(attended, n) / n)
+                users_list.append(tuple(user_tup))
+        except Exception as e:
+            # trace error - print("Login failed, error: {0}".format(e))
+            pass
+
+    return users_list
+
 
 # helpers
+def count_attendances_in_last_n_weeks(attended, n):
+    count = 0
+    last_date = datetime.now() - timedelta(weeks=n)
+    for d in attended.values():
+        converted_date = datetime.strptime(d, "%Y-%m-%dT%H:%M:%S.%fZ")
+        if converted_date > last_date:
+            count += 1
+
+    return count
+
+
 def get_last_attended(attended):
     return max(d for d in attended.values())
